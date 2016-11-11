4b61cfa9d9f31aa5029e3007ee09fd02643cfb70
hariharaselvam <hariharaselvam8@gmail.com>
2016-11-11 15:34:13 +0530

    static files

--- src/wakeup/ui/urls.py
+++ src/wakeup/ui/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from ui import views
 
 urlpatterns = [
--- src/wakeup/wakeup/settings.py
+++ src/wakeup/wakeup/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ui',
 ]
 
 MIDDLEWARE = [
@@ -113,8 +114,15 @@ USE_L10N = True
 
 USE_TZ = True
 
+PWD = os.path.dirname(os.path.realpath(__file__))
+
+MEDIA_ROOT = os.path.join(PWD, "../media")
+
+MEDIA_URL = ''
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
