e96f53fbd37ad4ebc42737f21b113842ce5bd3d2
hariharaselvam <hariharaselvam8@gmail.com>
2016-11-12 12:17:47 +0530

    API code reformatted

--- src/wakeup/ui/api.py
+++ src/wakeup/ui/api.py
@@ -1,9 +1,5 @@
-from django.contrib.auth import get_user_model
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.viewsets import ModelViewSet,ViewSet
+
+from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import status
 
