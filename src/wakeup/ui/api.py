264bbd352fa75f24624719bfe3384dccf6ee6548
hariharaselvam <hariharaselvam8@gmail.com>
2016-11-12 12:10:15 +0530

    API with authentication

--- src/wakeup/ui/api.py
+++ src/wakeup/ui/api.py
@@ -1,7 +1,9 @@
 from django.contrib.auth import get_user_model
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet,ViewSet
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
@@ -13,4 +15,24 @@ User = get_user_model()
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class SystemInfoViewSet(ViewSet):
+    # base_url = r'/(?P<pk>\d+)/cpu'
+    base_url = r'/systeminfo'
+    base_name = 'session'
+
+    def create(self, request):
+        return Response(None, status=status.HTTP_400_BAD_REQUEST)
+
+    def list(self, request):
+        if not request.user.is_authenticated():
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+        if request.method == "GET":
+            return Response({"revision": "v1"})
+
+    def retrieve(self, request, code=None):
+        if not request.user.is_authenticated():
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
 
--- src/wakeup/wakeup/urls.py
+++ src/wakeup/wakeup/urls.py
@@ -19,14 +19,37 @@ from django.contrib import admin
 from django.views.static import serve
 from rest_framework.routers import SimpleRouter
 from ui.api import *
-router = SimpleRouter()
-router.register('users',UserViewSet)
+import inspect
+#router = SimpleRouter()
+#router.register('users',UserViewSet)
+#router.register('session',SystemInfoViewSet)
+
+def module_import(name):
+    """
+    If you need to import a module on the fly, this helper function assists
+    you.  So say you want to import something like spam.eggs.are.cool, this
+    will sort out the full import.
+    """
+    mod = __import__(name, globals(), locals(), [], -1)
+    components = name.split('.')
+    for comp in components[1:]:
+        mod = getattr(mod, comp)
+    return mod
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('ui.urls')),
-    url(r'^api/v1/', include(router.urls))
+    url(r'^api/', include('api.urls')),
+    #url(r'^api/v1/', include(router.urls))
 ]
+
+module = module_import("ui")
+for name, obj in inspect.getmembers(module):
+    if name == 'dynamicurlpatterns':
+        if len(obj) > 0:
+            urlpatterns += obj
+#urlpatterns += SystemInfoViewSet
+
 urlpatterns += [
         url(r'^media/(?P<path>.*)$', serve, {
             'document_root': settings.MEDIA_ROOT,
